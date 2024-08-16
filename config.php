510852674e68f9b754241c78272aac57db24f594
HugoMartineau <hugo@helloasso.com>
2024-08-16 15:38:44 +0200

    set database connexion and table creation

--- config.php
+++ config.php
@@ -1,24 +1,37 @@
 <?php
-
 require 'vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->safeLoad();
+$dotenv->load();
+
+// Déterminer l'environnement actuel
+$isLocal = $_ENV['DB_ENV'] === 'local';
 
 $ADMIN_USER = $_ENV['ADMIN_USER'];
 $ADMIN_PASSWORD = $_ENV['ADMIN_PASSWORD'];
 
-$options = array(
-	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-	PDO::MYSQL_ATTR_SSL_CA => '',
-	PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-);
+// Configurer les paramètres de connexion en fonction de l'environnement
+$host = $isLocal ? $_ENV['DBURL_LOCAL'] : $_ENV['DBURL'];
+$dbname = $isLocal ? $_ENV['DBNAME_LOCAL'] : $_ENV['DBNAME'];
+$user = $isLocal ? $_ENV['DBUSER_LOCAL'] : $_ENV['DBUSER'];
+$password = $isLocal ? $_ENV['DBPASSWORD_LOCAL'] : $_ENV['DBPASSWORD'];
 
-$db = new PDO(
-	'mysql:host=' . $_ENV['DBURL'] . ';dbname=' . $_ENV['DBNAME'] . ';charset=utf8mb4',
-	$_ENV['DBUSER'],
-	$_ENV['DBPASSWORD'] ?? null,
-	$options
-);
+// Options de connexion
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
 
+try {
+    $db = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $password,
+        $options
+    );
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
 ?>
+
+
