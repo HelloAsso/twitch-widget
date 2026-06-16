2f4bebf230a62cd8626cc7bb3eb4a4838b36de74
Léo <leo.maisonneuve@gmail.com>
2026-06-16 19:50:58 +0200

    Add goal list

--- migrations/17-add-goals-table.sql
+++ migrations/17-add-goals-table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE sandbox_goals (
+CREATE TABLE {prefix}goals (
     id INT AUTO_INCREMENT PRIMARY KEY,
     charity_stream_guid VARCHAR(255) NULL,
     charity_event_guid VARCHAR(255) NULL,
@@ -7,11 +7,11 @@ CREATE TABLE sandbox_goals (
     INDEX idx_event_guid (charity_event_guid)
 );
 
-INSERT INTO sandbox_goals (charity_stream_guid, amount)
-SELECT guid, goal FROM sandbox_charity_stream WHERE goal > 0;
+INSERT INTO {prefix}goals (charity_stream_guid, amount)
+SELECT guid, goal FROM {prefix}charity_stream WHERE goal > 0;
 
-INSERT INTO sandbox_goals (charity_event_guid, amount)
-SELECT guid, goal FROM sandbox_charity_event WHERE goal > 0;
+INSERT INTO {prefix}goals (charity_event_guid, amount)
+SELECT guid, goal FROM {prefix}charity_event WHERE goal > 0;
 
-ALTER TABLE sandbox_charity_stream DROP COLUMN goal;
-ALTER TABLE sandbox_charity_event DROP COLUMN goal;
+ALTER TABLE {prefix}charity_stream DROP COLUMN goal;
+ALTER TABLE {prefix}charity_event DROP COLUMN goal;
